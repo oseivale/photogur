bdbf73bfa2dceb7bb61b6ddd0c7b2a901ac566cc
Valerie Osei <valerie.osei@gmail.com>
2018-08-07 17:04:45 -0400

    created form, tested params hash

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,4 +7,12 @@ class PicturesController < ApplicationController
     @picture = Picture.find(params[:id])
   end
 
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    render text: "Received POST request to '/pictures' with the data"
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
-Rails.application.routes.draw do
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
 
 Rails.application.routes.draw do
   get 'pictures' => 'pictures#index'
 
-end
+  post 'pictures' => 'pictures#create'
+  get 'pictures/new' => 'pictures#new'
 
-Rails.application.routes.draw do
-  get 'pictures' => 'pictures#index'
   get 'pictures/:id' => 'pictures#show'
 
 end
